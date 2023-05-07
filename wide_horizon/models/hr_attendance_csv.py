352c6efcd823c4bc609ff63acf2ed89343b6cf67
OmarDoukmakAltanmya <130458048+OmarDoukmakAltanmya@users.noreply.github.com>
2023-05-07 16:54:20 +0300

    Add files via upload

--- wide_horizon/models/hr_attendance_csv.py
+++ wide_horizon/models/hr_attendance_csv.py
@@ -9,13 +9,26 @@ class HrAttendanceCSV(models.Model):
     @api.depends('employee_id')
     def _compute_emp(self):
         for rec in self:
-            rec.emp = None
             if rec.reg_number:
                 rec.employee_id = self.env['hr.employee'].search([('registration_number', '=', rec.reg_number)])
+            temp_employee = self.env['hr.employee'].search([('id', '>', -1)])
+            for temp_emp in temp_employee:
+                if temp_emp.name.isdigit():
+                    if temp_emp.registration_number == rec.reg_number:
+                        temp_emp.unlink()
+            rec.emp = None
 
 
 class HrEmployeeCSV(models.Model):
     _inherit = 'hr.employee'
+
+    @api.model
+    def search_read(self, domain=None, fields=None, offset=0, limit=None, order=None):
+        temp_employee = self.env['hr.employee'].search([('id', '>', -1)])
+        for temp_emp in temp_employee:
+            if temp_emp.name.isdigit():
+                temp_emp.unlink()
+        return super().search_read(domain=domain, fields=fields, offset=offset, limit=limit, order=order)
 
     def name_get(self):
         result = []
